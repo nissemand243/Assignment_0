955b666e20554fe0b43874a113e6df48a3a6852c
nissemand243 <xander.s234@gmail.com>
2021-09-05 22:38:03 +0200

    bugfixing some issues with auto generated files

--- Task_4.Tests/UnitTest1.cs
+++ Task_4.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Task_4.Tests
@@ -6,9 +7,18 @@ namespace Task_4.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Main_Prints_Hello_World()
         {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
 
+            // Act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            // Assert
+            Assert.Equal("Hello, World!", output);
         }
     }
 }
