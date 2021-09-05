24233498786b55a40eefa94392cae8e640c2c464
nissemand243 <47781194+nissemand243@users.noreply.github.com>
2021-09-05 23:02:42 +0200

    Task 5 - 6 done (#2)
    * added console app project and xunit library
    
    * added directory structure and task 5 base
    
    * remade the folder structure and updated Readme
    
    * Completed Task4
    
    * updated readme
    
    * Tested and Implemented IsLeapYear
    
    * Tested and implemented user interface for IsLeapYear
    
    * trying bugfix
    
    * bugfixing some issues with auto generated files
    
    * bugfixing some issues with auto generated files

---
+++ Leap_Year.Test/UnitTest1.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace Leap_Year.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Year1400_Is_Not_Leap()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("1400");
+            Console.SetIn(input);
+
+            // Act
+            Program.Main(new string[0]);
+
+            // Assert
+            string[] output = writer.ToString().Split(Environment.NewLine);
+
+            bool isLeap = Program.IsLeapYear(1400);
+            Assert.False(isLeap);
+
+            Assert.Equal("Please enter a year", output[0].Trim());
+            Assert.Equal("oh no, it is not a leap year", output[1].Trim());
+
+        }
+
+        [Fact]
+        public void Year1600_Is_Leap()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("1600");
+            Console.SetIn(input);
+
+            // Act
+            Program.Main(new string[0]);
+
+            // Assert
+            string[] output = writer.ToString().Split(Environment.NewLine);
+            bool isLeap = Program.IsLeapYear(1600);
+            
+            Assert.True(isLeap);
+            
+            Assert.Equal("Please enter a year", output[0].Trim());
+            Assert.Equal("yay, it is a leap year", output[1].Trim());
+
+        }
+    }
+}
+
