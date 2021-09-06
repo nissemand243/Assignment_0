1392e28eb99bf62e1dc6f69395a538c898f867e6
nissemand243 <xander.s234@gmail.com>
2021-09-06 12:19:20 +0200

    Editted signature of code to fix Idiomatic C#

--- Leap_Year/Program.cs
+++ Leap_Year/Program.cs
@@ -12,25 +12,34 @@ namespace Leap_Year
             int res = Convert.ToInt32(input);
 
             bool LeapYear = IsLeapYear(res);
-            if(LeapYear){
+            if(LeapYear)
+            {
                 Console.WriteLine("yay, it is a leap year");
-            }else{
+            }else
+            {
                 Console.WriteLine("oh no, it is not a leap year");
             }
         }
 
-        public static bool IsLeapYear(int year){
-            if(year % 4 == 0){
-                if(year % 100 == 0){
-                    if(year % 400 == 0){
+        public static bool IsLeapYear(int year)
+        {
+            if(year % 4 == 0)
+            {
+                if(year % 100 == 0)
+                {
+                    if(year % 400 == 0)
+                    {
                         return true;
-                    }else{
+                    }else
+                    {
                         return false;
                     }
-                }else{
+                }else
+                {
                     return true;
                 }
-            }else{
+            }else
+            {
                 return false;
             }
         }
