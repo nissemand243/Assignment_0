6673f23734a137094e50c23ff8a9b30fcadcd5af
nissemand243 <xander.s234@gmail.com>
2021-09-06 13:32:13 +0200

    tested and implemented error handling

--- Leap_Year.Test/UnitTest1.cs
+++ Leap_Year.Test/UnitTest1.cs
@@ -7,13 +7,13 @@ namespace Leap_Year.Test
     public class UnitTest1
     {
         [Fact]
-        public void Year1400_Is_Not_Leap()
+        public void Year1700_Is_Not_Leap()
         {
             // Arrange
             var writer = new StringWriter();
             Console.SetOut(writer);
 
-            var input = new StringReader("1400");
+            var input = new StringReader("1700");
             Console.SetIn(input);
 
             // Act
@@ -22,7 +22,7 @@ namespace Leap_Year.Test
             // Assert
             string[] output = writer.ToString().Split(Environment.NewLine);
 
-            bool isLeap = Program.IsLeapYear(1400);
+            bool isLeap = Program.IsLeapYear(1700);
             Assert.False(isLeap);
 
             Assert.Equal("Please enter a year", output[0].Trim());
@@ -71,7 +71,27 @@ namespace Leap_Year.Test
             string[] output = writer.ToString().Split(Environment.NewLine);
             
             Assert.Equal("Please enter a year", output[0].Trim());
-            Assert.Equal("Input year below 1582 is invalid", output[1].Trim());
+            Assert.Equal("Input is invalid. Please input year from 1582 and fourth", output[1].Trim());
+        }
+
+        [Fact]
+        public void IsLeapYear_NonInt_Input_Is_Invalid()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("asdasd");
+            Console.SetIn(input);
+
+            // Act
+            Program.Main(new string[0]);
+
+            // Assert
+            string[] output = writer.ToString().Split(Environment.NewLine);
+            
+            Assert.Equal("Please enter a year", output[0].Trim());
+            Assert.Equal("Letters and decimal numbers are not allowed. Please input year in integer format.", output[1].Trim());
         }
     }
 }
