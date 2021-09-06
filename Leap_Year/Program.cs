38596ff1656f7c3a7581176385bd50752c330bac
nissemand243 <47781194+nissemand243@users.noreply.github.com>
2021-09-06 13:41:37 +0200

    Task 7 (#4)
    * added test case for task_7, failed
    
    * tested and implemented error handling

--- Leap_Year/Program.cs
+++ Leap_Year/Program.cs
@@ -9,20 +9,32 @@ namespace Leap_Year
         {
             Console.WriteLine("Please enter a year");
             string input = Console.ReadLine();
-            int res = Convert.ToInt32(input);
+            int res;
+
+            bool convert_to_int = Int32.TryParse(input, out res);
 
             bool LeapYear = IsLeapYear(res);
-            if(LeapYear)
+            if(convert_to_int)
             {
-                Console.WriteLine("yay, it is a leap year");
-            }else
-            {
-                Console.WriteLine("oh no, it is not a leap year");
+               if(res < 1582)
+                {
+                    Console.WriteLine("Input is invalid. Please input year from 1582 and fourth");
+                }else if(LeapYear)
+                {
+                    Console.WriteLine("yay, it is a leap year");
+                }else
+                {
+                    Console.WriteLine("oh no, it is not a leap year");
+                } 
+            }else{
+                Console.WriteLine("Letters and decimal numbers are not allowed. Please input year in integer format.");
             }
+            
         }
 
         public static bool IsLeapYear(int year)
         {
+            
             if(year % 4 == 0)
             {
                 if(year % 100 == 0)
