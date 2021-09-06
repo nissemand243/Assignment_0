ae1b5baae5334ef88807268f82e86d05f5e75c90
nissemand243 <xander.s234@gmail.com>
2021-09-06 12:34:05 +0200

    added test case for task_7, failed

--- Leap_Year.Test/UnitTest1.cs
+++ Leap_Year.Test/UnitTest1.cs
@@ -53,6 +53,26 @@ namespace Leap_Year.Test
             Assert.Equal("yay, it is a leap year", output[1].Trim());
 
         }
+
+        [Fact]
+        public void IsLeapYear1500_Input_Not_Valid()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("1500");
+            Console.SetIn(input);
+
+            // Act
+            Program.Main(new string[0]);
+
+            // Assert
+            string[] output = writer.ToString().Split(Environment.NewLine);
+            
+            Assert.Equal("Please enter a year", output[0].Trim());
+            Assert.Equal("Input year below 1582 is invalid", output[1].Trim());
+        }
     }
 }
 
